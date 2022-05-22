c0024db09f046f7f50e8e9c9a1d3105864d9dce3
prashanth <prashanth1krishnamurthy@gmail.com>
2022-05-22 20:53:42 +0530

    Add project files.

---
+++ DemoBudget-AspDapper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoBudget_AspDapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
